cc1aa5591e577f630f99301d59e9a6bfe2d9cd81
Joseph Chandler <josephalanchandlerjr@gmail.com>
2018-01-04 12:25:59 -0500

    add two cats, each with own click counter

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
-const cat = document.querySelector('#cat');
-const catCount = document.querySelector('#cat-count');
-var clickCount = 0;
 
-cat.addEventListener('click', function clickHandler(){
-	const catCount = document.querySelector('#cat-count');
-	clickCount++;
-	catCount.innerText = clickCount; 
-	});
+function makeCatClickHandler(cat, catCounter){
+	var clickCount = 0;
+	return function(){
+		clickCount++;
+		catCounter.innerText = clickCount; 
+		};
+};
+console.log(document.querySelector('#cat-count1'));
+const cat1 = document.querySelector('#cat1');
+const catOneHandler = makeCatClickHandler(document.querySelector('#cat1'),document.querySelector('#cat-count1'));
+cat1.addEventListener('click', catOneHandler);
+
+const cat2 = document.querySelector('#cat2');
+const catTwoHandler = makeCatClickHandler(document.querySelector('#cat2'),document.querySelector('#cat-count2'));
+cat2.addEventListener('click', catTwoHandler);
