070a6afb16699f55f7412e43b38b8af69ce96e92
Joseph Chandler <josephalanchandlerjr@gmail.com>
2018-01-17 14:59:15 -0500

    fix object names

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
 
-var Model = {
+var model = {
 	catNames: ['Scruffy','Mr. Fluffs','Sugar & Spice','Tiger','Lydia'],
 	init: function(){
 		this.catList = [];
 		this.catNames.forEach(function(cat, ix){ 
-			Model.catList.push({
+			model.catList.push({
 				name: cat,
 				number: ix,
 				clicks: 0
 			});
 		});
-	}
+	},
+	getCats: function() { return this.catList; }
 };
 
-var Octopus = {
+var octopus = {
 	init: function(){
-		Model.init();
-		CatMenuView.init(Model.catNames); 
-		CatView.init(Model.catList); 
+		model.init();
+		menuView.init(model.catNames); 
+		view.init(model.catList); 
 	},
 	update: function(newCatNum){  
-		CatView.update(Model.catList, newCatNum); 
+		view.update(model.catList, newCatNum); 
 	},
 	updateView: function(catNum){
-		Model.catList[catNum].clicks++;
-		CatView.update(Model.catList, catNum);
+		model.catList[catNum].clicks++;
+		view.update(model.catList, catNum);
 	}
-
 }
 	
-var CatMenuView = { 			
-	init: function(catList) {	//
+var menuView = { 			
+	init: function(catList) {
 		this.catMenu = document.querySelector('#catMenu');
-		catList.forEach(function(elem, ix){ CatMenuView.createCatMenuItem(elem,ix);});
+		catList.forEach(function(elem, ix){ menuView.createCatMenuItem(elem,ix);});
 		this.catMenu.addEventListener('change', function (event) {
-			Octopus.update(event.target.value);
+			octopus.update(event.target.value);
 		});
 	},
 	createCatMenuItem: function(catName, catNum){
@@ -45,8 +45,7 @@ var CatMenuView = {
 	}
 };
 
-var CatView = {
-	catList: [],
+var view = {
 	container: document.querySelector('.container'),
 	init: function(catList){
 		this.render(catList, 0);
@@ -67,22 +66,19 @@ var CatView = {
 		catImg.setAttribute('src', 'src/cat'+catNum+'.jpg');
 		catImg.setAttribute('alt', 'kitty cat');
 		
-		var catCount = document.createElement('p');
+		var catCount = document.createElement('p')     // make a p
 		catCount.innerText = cat.name + ' ' + cat.clicks;     
 		catCount.classList.add('cat-count');
-		var clickHandler = this.makeCatClickHandler(catNum, catCount);
-		catDiv.addEventListener('click', clickHandler);
+		catDiv.addEventListener('click', this.makeCatClickHandler(catNum));
 
 		catDiv.appendChild(catImg);	
 		catDiv.appendChild(catCount);		
 		return catDiv;
 	},
-	makeCatClickHandler: function(ix, catCounter){
-		return function(){
-			Octopus.updateView(ix);
-			};
+	makeCatClickHandler: function(ix){
+		return function(){ octopus.updateView(ix);};
 	},
 };
 
-Octopus.init();
+octopus.init();
 
